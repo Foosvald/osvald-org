6a7ebf6ce739f98f00a25b2d680b810a5b61e8de
Osvald Ivarsson <osvald.ivarsson@netinsight.net>
2022-03-02 08:00:01 +0100

    Optimize the wasm size in release mode.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 extern crate yew;
+extern crate wee_alloc;
+
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 use yew::prelude::*;
 use yew_router::prelude::*;
